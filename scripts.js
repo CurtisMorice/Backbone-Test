3547453e85c6d6b4c13188c7ae2706fd77b4798b
Curtis Morice <curtismorice@gmail.com>
2018-09-16 10:29:26 -0500

    added img capability and updated ad edit

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 //Backbone Model is a model or class object
 let Track = Backbone.Model.extend({
     defaults: {
-        trackNumber: '',
+        image: "",
         name:'',
         description:'',
         by:''
@@ -11,10 +11,10 @@ let Track = Backbone.Model.extend({
 //Backbone Collection is a an [] of Models
 let Playlists = Backbone.Collection.extend({});
 
-//instantiate 2 tracks
+// instantiate 2 tracks
 
 let track1 = new Track({
-    trackNumber: '01',
+    image: 'logo_2x_SISYPHUS.png',
     name:'Sine',
     description:'a bunch of lines all over the place.',
     by:'Sisyphus Industries',
@@ -22,7 +22,7 @@ let track1 = new Track({
 });
 
 let track2 = new Track({
-    trackNumber: '02',
+    image: 'sisyphus-cardlogo.png',
     name:'Erase',
     description:'clears the board for the nex design.',
     by:'Sisyphus Industries',
@@ -31,7 +31,6 @@ let track2 = new Track({
 //created a collection
 let playlists = new Playlists([track1, track2]); 
 
-
 //Backbone View for 1 track
 let TrackView = Backbone.View.extend({
     model: new Track(),
@@ -39,6 +38,37 @@ let TrackView = Backbone.View.extend({
     initialize: function() {
         this.template = _.template($('.track_template').html());
     },
+    events: {
+        'click .edit_track': 'edit',
+        'click .update_track': 'update',
+
+
+    },
+    edit: function(){
+         $('.edit_track').hide();
+         $('.delete_track').hide();
+         $('.update_track').show();
+         $('.cancel').show();
+
+         let image = $('.image').html();
+         let name = $('.name').html();
+         let description = $('.description').html();
+         let by = $('.by').html();
+
+         this.$('.image').html('<input type="file" accept="image/*" class="form-control image_update" value="' + image + '">');
+         this.$('.name').html('<input type="text" class="form-control name_update" value="' + name + '">');
+         this.$('.description').html('<input type="text" class="form-control description_update" value="' + description + '">');
+         this.$('.by').html('<input type="text" class="form-control by_update" value="' + by + '">');
+     
+
+    },
+    update: function(){
+        this.model.set('image', $('image_update').val());
+        this.model.set('name', $('.name_update').val());
+        this.model.set('description', $('.description_update').val());
+        this.model.set('by', $('.by_update').val());
+
+    }
     render: function() {
         this.$el.html(this.template(this.model.toJSON()));
         return this;
@@ -66,11 +96,15 @@ let playlistView  = new PlaylistView();
 $(document).ready(function() {
     $('.add_track').on('click', function() {
         let track = new Track({
-            trackNumber: $('.track_input').val(),
+            image: $('.image_input').val(),
             name: $('.name_input').val(),
             description: $('.description_input').val(),
             by: $('.by_input').val()
         });
+        $('.image_input').val('');
+        $('.name_input').val('');
+        $('.description_input').val('');
+        $('.by_input').val('');
         console.log(track.toJSON());
         playlists.add(track);
     })
